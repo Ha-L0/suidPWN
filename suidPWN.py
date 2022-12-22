e4746f12d7a33e214dee20ab26c5228810f970cf
rndinfosecguy <hy3n4@protonmail.com>
2022-12-22 09:50:20 +0100

    minor output changes

--- suidPWN.py
+++ suidPWN.py
@@ -5,10 +5,10 @@ import argparse
 import html
 
 programepilog = "example usage: python3 " + sys.argv[0]
-programdescription = "Search for suid binaries allowing privilege escalation. "
+programdescription = "Identify SUID binaries allowing privilege escalation. "
 programdescription += "Tool made by H4&L0. Credits for LPE techniques to https://gtfobins.github.io/"
 parser = argparse.ArgumentParser(description=programdescription, epilog=programepilog)
-parser.add_argument("-fd", "--find", help="print find suid binaries string", action="store_true", required=False)
+parser.add_argument("-fd", "--find", help="print find suid binaries command", action="store_true", required=False)
 parser.add_argument("-f", "--file", help="read suid files result from file", required=False)
 parser.add_argument("-c", "--credits", help="show credits", action="store_true", required=False)
 args = parser.parse_args()
@@ -20,9 +20,7 @@ if args.find:
 
 if args.credits:
     creds = "\nWe made this tool while working and different CTF boxes.\n"
-    creds += "The reason was to fast identify SUID binaries which are vulnerable to an LPE.\n"
-    creds += "We basically just scraped the binaries from the gtfobins website, stored them into a " \
-             "text file and wrote this script to check SUID binaries from the target against this text file.\n"
+    creds += "The reason was to fast identify SUID binaries which are vulnerable to an LPE.\n\n"
     creds += "All the credits of the local privilege escalation techniques go to gtfobins. These guys are awesome! <3"
     print(creds)
     sys.exit(0)
